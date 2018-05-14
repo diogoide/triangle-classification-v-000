6a7d5962e39fbeb3b166806fb3f76a81b8d0ad72
Diogo Ide <diogoide@gmail.com>
2018-05-14 00:05:40 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,12 +8,12 @@ class Triangle
   end
 
   def kind
-    "equilateral" if @side1_length != 0 && @side1_length == @side2_length == @side3_length
+    "equilateral" if @side1_length != 0 && @side1_length == @side2_length && @side1_length == @side3_length
     "isosceles" if  @side1_length != 0 && @side2_length != 0 && @side3_length != 0 && @side1_length == @side2_length || @side1_length == @side3_length || @side2_length == @side3_length
     "scalene" if @side1_length != 0 && @side2_length != 0 && @side3_length != 0 && @side1_length != @side2_length && @side1_length != @side3_length && @side2_length != @side3_length
           end
         end
-      end 
+      end
   end
 
 end
