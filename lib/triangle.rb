6d3124d71924f1f9fa69b13e5f3c7963c958375b
Diogo Ide <diogoide@gmail.com>
2018-05-14 00:05:07 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,13 +8,12 @@ class Triangle
   end
 
   def kind
-    if @side1_length != 0 && @side1_length == @side2_length == @side3_length
-      "equilateral"
-    elsif  @side1_length != 0 && @side2_length != 0 && @side3_length != 0 && @side1_length == @side2_length || @side1_length == @side3_length || @side2_length == @side3_length
-      "isosceles"
-    elsif @side1_length != 0 && @side2_length != 0 && @side3_length != 0 && @side1_length != @side2_length && @side1_length != @side3_length && @side2_length != @side3_length
-      "scalene"
-    end
+    "equilateral" if @side1_length != 0 && @side1_length == @side2_length == @side3_length
+    "isosceles" if  @side1_length != 0 && @side2_length != 0 && @side3_length != 0 && @side1_length == @side2_length || @side1_length == @side3_length || @side2_length == @side3_length
+    "scalene" if @side1_length != 0 && @side2_length != 0 && @side3_length != 0 && @side1_length != @side2_length && @side1_length != @side3_length && @side2_length != @side3_length
+          end
+        end
+      end 
   end
 
 end
